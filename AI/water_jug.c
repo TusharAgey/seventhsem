f9ab5b5a191c56c417110e36dec7ccf45bcc8ef5
Tushar Agey <agey.tushar3@gmail.com>
2017-08-06 09:22:34 +0530

    improved water_jug code

--- AI/water_jug.c
+++ AI/water_jug.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 typedef struct jugs{
 	int jug_capacity;
 	int jug_water;
@@ -7,27 +9,51 @@ typedef struct jugs{
 jug m, n;
 void solve(int d){ //d is the required ltr of water in either of the jug
 	int x;
-	printf("%dltr jug\t%dltr jug\n", m.jug_capacity, n.jug_capacity);
-	while(m.jug_water != d && n.jug_water != d){
-		if(m.jug_water == m.jug_capacity)
-			m.jug_water = 0;
-		if(n.jug_water == 0)
-			n.jug_water = n.jug_capacity;
-		x = m.jug_capacity - m.jug_water;//free space in smaller jug
-		if(n.jug_water >= x){
-			m.jug_water += x; //pour it to smaller jug
-			n.jug_water -= x; //hence removed from larger jug
+	srand(time(NULL));
+	int choice = rand() % 2 + 1;//increase the randomness
+	printf("%dltr %dltr\n", m.jug_capacity, n.jug_capacity);
+	if(choice == 1)
+		while(m.jug_water != d && n.jug_water != d){
+			if(m.jug_water == m.jug_capacity)
+				m.jug_water = 0;
+			if(n.jug_water == 0)
+				n.jug_water = n.jug_capacity;
+			printf("| %d | %d |\n", m.jug_water, n.jug_water);
+			x = m.jug_capacity - m.jug_water;//free space in smaller jug
+			if(n.jug_water >= x){
+				m.jug_water += x; //pour it to smaller jug
+				n.jug_water -= x; //hence removed from larger jug
+			}
+			else{
+				m.jug_water += n.jug_water; //pour everything remaining
+				n.jug_water -= n.jug_water;//hence removed
+			}
+			printf("| %d | %d |\n", m.jug_water, n.jug_water);
 		}
-		else{
-			m.jug_water += n.jug_water; //pour everything remaining
-			n.jug_water -= n.jug_water;//hence removed
+	else{
+		while(m.jug_water != d && n.jug_water != d){
+			if(n.jug_water == n.jug_capacity)
+				n.jug_water = 0;
+			if(m.jug_water == 0)
+				m.jug_water = m.jug_capacity;
+			printf("| %d | %d |\n", m.jug_water, n.jug_water);
+			x = n.jug_capacity - n.jug_water;//free space in larger jug
+			if(m.jug_water >= x){
+				n.jug_water += x; //pour it to larger jug
+				m.jug_water -= x; //hence removed from smaller jug
+			}
+			else{
+				n.jug_water += m.jug_water; //pour everything remaining
+				m.jug_water -= m.jug_water; //hence removed
+			}
+			printf("| %d | %d |\n", m.jug_water, n.jug_water);
 		}
-		printf("%d\t%d\n", m.jug_water, n.jug_water);
 	}
-	//fill n
-	//put it into m
-	//if m is full, empty it
-	//if n is empty, full it
+}
+int gcd(int no1, int no2){
+	 if (no2 == 0)
+       return no1;
+    return gcd(no2, no1 % no2);
 }
 int main(){
 	int x, d;
@@ -38,6 +64,14 @@ int main(){
 		x = m.jug_capacity;
 		m.jug_capacity = n.jug_capacity;
 		n.jug_capacity = x;
+	}
+	if(n.jug_capacity < d){
+		printf("Solution is not possible\n");
+		return 1;
+	}
+	if((d % gcd(m.jug_capacity, n.jug_capacity)) != 0){
+		printf("Solution is not possible\n");
+		return 1;
 	}
 	solve(d);
 	return 0;
