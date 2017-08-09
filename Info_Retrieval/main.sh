a62338a7f52c633de9309cb32531ecf4194f592b
Tushar Agey <agey.tushar3@gmail.com>
2017-08-09 07:14:04 +0530

    simple modifications

--- Info_Retrieval/main.sh
+++ Info_Retrieval/main.sh
@@ -1,3 +1,4 @@
+rm output.txt
 timeit=$(date "+%Y-%m-%d-%H:%M:%S") #unique milestone for this search
 len=$(ls -p $1 | grep -v /  | wc -l) #$1 denotes the directory containing documents to be indexed and searched
 #Database table header	time_it	file_name	unique_word	count_it //time is to milestone the run
@@ -12,9 +13,9 @@ do
 	do
 		oldthings=$thing #preserving the original document word
 		thing=$(echo "from stemming.porter2 import stem ; print stem('$thing')" | python) #apply stemming algorithm using python
-		if [ $( cat "./input/stopword.txt" | grep -i $oldthings | wc -c ) -eq 0 ]; #pick non-stopwords only
+		if [ $( cat "./input/stopword.txt" | grep -i $thing | wc -c ) -eq 0 ]; #pick non-stopwords only
 		then
-			count=$(grep -o " $oldthings " <<< "$data" | wc -l) #number of occurances
+			count=$(grep -o " $thing " <<< "$data" | wc -l) #number of occurances
 			statement="insert into word_count(time_it, file_name, unique_word, count_it) values ('$timeit', '$filen', '$thing', $count );" #preparing sql statements
 			echo $statement >> tmp.sql #saving sql statements
 		fi
@@ -26,4 +27,11 @@ mysql ir_assignment_1 -uroot -hlocalhost -p < tmp2.sql
 #done running sql
 rm tmp.sql
 rm tmp2.sql
-/Applications/Firefox.app/Contents/MacOS/firefox localhost/mysite/IR/Assignment1 & #invoke query processor ui
+#/Applications/Firefox.app/Contents/MacOS/firefox localhost/mysite/IR/Assignment1 & #invoke query processor ui
+words=$(echo "select distinct unique_word from word_count;" |mysql -sN ir_assignment_1 -uroot -hlocalhost -pqwerty1@)
+for word in $words;
+do
+	data=$(echo "select file_name, count_it from word_count where unique_word = '$word'" | mysql -sN ir_assignment_1 -uroot -hlocalhost -pqwerty1@)
+	echo $word >> output.txt
+	echo '['$data']' >> output.txt
+done
