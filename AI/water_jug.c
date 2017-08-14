5ee601f068974e37e07a5736627ba9b90b2b3325
Tushar Agey <agey.tushar3@gmail.com>
2017-08-14 07:17:14 +0530

    improved water_jug solution

--- AI/water_jug.c
+++ AI/water_jug.c
@@ -1,71 +1,73 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
 typedef struct jugs{
 	int jug_capacity;
 	int jug_water;
 }jug;
 jug m, n;
-void solve(int d){ //d is the required ltr of water in either of the jug
-	int x;
-	srand(time(NULL));
-	int choice = rand() % 2 + 1;//increase the randomness
-	printf("%dltr %dltr\n", m.jug_capacity, n.jug_capacity);
-	if(choice == 1)
-		while(m.jug_water != d && n.jug_water != d){
-			if(m.jug_water == m.jug_capacity){
-				m.jug_water = 0;
-				printf("small jug is full, empty it:\n");
-			}
-			if(n.jug_water == 0){
-				n.jug_water = n.jug_capacity;
-				printf("larger jug is empty, fill it:\n");
-			}
-			printf("| %d | %d |\n", m.jug_water, n.jug_water);
-			x = m.jug_capacity - m.jug_water;//free space in smaller jug
-			if(n.jug_water >= x){
-				m.jug_water += x; //pour it to smaller jug
-				n.jug_water -= x; //hence removed from larger jug
-			}
-			else{
-				m.jug_water += n.jug_water; //pour everything remaining
-				n.jug_water -= n.jug_water;//hence removed
-			}
-			printf("pour from larger jug to smaller one:\n");
-			printf("| %d | %d |\n", m.jug_water, n.jug_water);
+void solve(int d){ //d is the required ltr of water in either of the jugs
+	int x, s1 = 0, s2 = 0;
+	FILE *fp1 = fopen("output1.txt", "w");
+	FILE *fp2 = fopen("output2.txt", "w");
+	fprintf(fp1, "%dltr %dltr\n", m.jug_capacity, n.jug_capacity);
+	while(m.jug_water != d && n.jug_water != d){
+		if(m.jug_water == m.jug_capacity){
+			m.jug_water = 0;
+			fprintf(fp1, "small jug is full, empty it:\n");
 		}
-	else{
-		while(m.jug_water != d && n.jug_water != d){
-			if(n.jug_water == n.jug_capacity){
-				n.jug_water = 0;
-				printf("larger jug is full, so empty it:\n");
-			}
-			if(m.jug_water == 0){
-				m.jug_water = m.jug_capacity;
-				printf("smaller jug is empty, so fill it:\n");
-			}
-			printf("| %d | %d |\n", m.jug_water, n.jug_water);
-			x = n.jug_capacity - n.jug_water;//free space in larger jug
-			if(m.jug_water >= x){
-				n.jug_water += x; //pour it to larger jug
-				m.jug_water -= x; //hence removed from smaller jug
-				
-			}
-			else{
-				n.jug_water += m.jug_water; //pour everything remaining
-				m.jug_water -= m.jug_water; //hence removed
-			}
-			printf("pour from smaller jug to larger one:\n");
-			printf("| %d | %d |\n", m.jug_water, n.jug_water);
+		if(n.jug_water == 0){
+			n.jug_water = n.jug_capacity;
+			fprintf(fp1, "larger jug is empty, fill it:\n");
 		}
+		fprintf(fp1, "| %d | %d |\n", m.jug_water, n.jug_water);
+		x = m.jug_capacity - m.jug_water;//free space in smaller jug
+		if(n.jug_water >= x){
+			m.jug_water += x; //pour it to smaller jug
+			n.jug_water -= x; //hence removed from larger jug
+		}
+		else{
+			m.jug_water += n.jug_water; //pour everything remaining
+			n.jug_water -= n.jug_water;//hence removed
+		}
+		fprintf(fp1, "pour from larger jug to smaller one:\n");
+		fprintf(fp1, "| %d | %d |\n", m.jug_water, n.jug_water);
+		s1++;
 	}
+	m.jug_water = n.jug_water = 0;
+	fprintf(fp2, "%dltr %dltr\n", m.jug_capacity, n.jug_capacity);
+	while(m.jug_water != d && n.jug_water != d){
+		if(n.jug_water == n.jug_capacity){
+			n.jug_water = 0;
+			fprintf(fp2, "larger jug is full, so empty it:\n");
+			}
+		if(m.jug_water == 0){
+			m.jug_water = m.jug_capacity;
+			fprintf(fp2, "smaller jug is empty, so fill it:\n");
+		}
+		fprintf(fp2, "| %d | %d |\n", m.jug_water, n.jug_water);
+		x = n.jug_capacity - n.jug_water;//free space in larger jug
+		if(m.jug_water >= x){
+			n.jug_water += x; //pour it to larger jug
+			m.jug_water -= x; //hence removed from smaller jug
+			
+		}
+		else{
+			n.jug_water += m.jug_water; //pour everything remaining
+			m.jug_water -= m.jug_water; //hence removed
+		}
+		fprintf(fp2, "pour from smaller jug to larger one:\n");
+		fprintf(fp2, "| %d | %d |\n", m.jug_water, n.jug_water);
+		s2++;
+	}
+	fclose(fp1);
+	fclose(fp2);
+	if(s1 < s2)//print optimised production rules followed
+		system("cat output1.txt");
+	else
+		system("cat output2.txt");
+	system("rm output1.txt output2.txt");//comment this statement to see output of both ways if followed
 	printf("\nyay! got the final state.\n");
-}
-int gcd(int no1, int no2){
-	 if (no2 == 0)
-       return no1;
-    return gcd(no2, no1 % no2);
 }
 int main(){
 	int x, d;
@@ -78,10 +80,6 @@ int main(){
 		n.jug_capacity = x;
 	}
 	if(n.jug_capacity < d){
-		printf("Solution is not possible\n");
-		return 1;
-	}
-	if((d % gcd(m.jug_capacity, n.jug_capacity)) != 0){
 		printf("Solution is not possible\n");
 		return 1;
 	}
