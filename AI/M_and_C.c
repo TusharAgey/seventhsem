487913803ef779cd34ea0be7cce909e7ee1b910b
Tushar Agey <agey.tushar3@gmail.com>
2017-08-31 11:26:08 +0530

    done with missionaries and cannibals problem

--- AI/M_and_C.c
+++ AI/M_and_C.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 struct myVector{
 	int missionaries;
@@ -6,29 +7,35 @@ struct myVector{
 	int boat;//1=I have boat & 0=I dont have boat
 }right_bank, left_bank;
 int d, doit = 1;//the boat capacity
+char tmpdata[100];
 void nextStep(){
 	if(right_bank.boat == 1){
 		if((left_bank.missionaries - (left_bank.cannibals + 1) >= 0 || left_bank.missionaries == 0) && right_bank.cannibals - 1 >= 0){ //send 1 cannibals to other side, if no side effect
 			left_bank.cannibals += 1;
 			right_bank.cannibals -= 1;
+			sprintf(tmpdata, "[one cannibal is sent to left bank]\n");
 		}
 		else if((left_bank.missionaries - (left_bank.cannibals + d) >= 0 || left_bank.missionaries == 0) && right_bank.cannibals - d >= 0){ //send 2 cannibals to other side, if no side effect
 			left_bank.cannibals += d;
 			right_bank.cannibals -= d;
+			sprintf(tmpdata, "[%d cannibals sent to left bank]\n", d);
 		}
 		else if((right_bank.missionaries - (right_bank.cannibals + d) >= 0 || left_bank.cannibals == 0) && right_bank.missionaries - d >= 0){ //send 2 missinaries to other side, if no side effect
 			right_bank.missionaries -= d;
 			left_bank.missionaries += d;
+			sprintf(tmpdata, "[%d missionaries sent to left bank]\n", d);
 		}
 		else if((right_bank.missionaries >= 1 && right_bank.cannibals >= 1 && right_bank.cannibals - 1 >= 0) && right_bank.missionaries - 1 >= 0){ //send a pair of missionari and cannibal to other side if available
 			left_bank.missionaries += 1;
 			left_bank.cannibals += 1;
 			right_bank.cannibals -= 1;
 			right_bank.missionaries -= 1;
+			sprintf(tmpdata, "[one missionary and one cannibal is sent to left bank]\n");
 		}
 		else if(right_bank.missionaries - (right_bank.cannibals + 1) >= 0 && right_bank.missionaries - 1 >= 0){ //send 1 missinaries to other side, if no side effect
 			right_bank.missionaries -= 1;
 			left_bank.missionaries += 1;
+			printf(tmpdata, "[One missionary is sent to left bank]\n");
 		}
 		right_bank.boat = 0;
 		left_bank.boat = 1;
@@ -37,6 +44,7 @@ void nextStep(){
 		if((right_bank.missionaries - (right_bank.cannibals + d) >= 0 || right_bank.missionaries == 0) && left_bank.cannibals - d >= 0 && doit == 1){ //send 2 cannibals to other side, if no side effect
 			left_bank.cannibals -= d;
 			right_bank.cannibals += d;
+			sprintf(tmpdata, "[%d cannibals sent to right bank]\n", d);
 			if(d == 3){
 				doit = 0;
 			}
@@ -44,11 +52,13 @@ void nextStep(){
 		else if((right_bank.cannibals <= right_bank.missionaries + d) && left_bank.missionaries - d >= 0){ //send 2 missinaries to other side, if no side effect
 			right_bank.missionaries += d;
 			left_bank.missionaries -= d;
+			sprintf(tmpdata, "[%d missinaries sent to right bank]\n", d);
 			doit = 1;
 		}
 		else if(left_bank.missionaries == 0){
 			if(left_bank.cannibals <= d){
 				right_bank.cannibals += left_bank.cannibals;
+				sprintf(tmpdata, "[%d cannibals sent to right bank]\n", left_bank.cannibals);
 				left_bank.cannibals = 0;
 			}
 		}
@@ -57,24 +67,31 @@ void nextStep(){
 			left_bank.cannibals -= d/2;
 			right_bank.cannibals += d/2;
 			right_bank.missionaries += d/2;
+			sprintf(tmpdata, "[%d cannibals and missionaries sent to right bank]\n", d/2);
 		}
 		else if(left_bank.missionaries >= 1 && left_bank.cannibals >= 1 && left_bank.missionaries - 1 >= 0 && left_bank.cannibals - 1 >= 0 && (right_bank.cannibals <= right_bank.missionaries + 1)	){ //send a pair of missionari and cannibal to other side if available
 			left_bank.missionaries -= 1;
 			left_bank.cannibals -= 1;
 			right_bank.cannibals += 1;
 			right_bank.missionaries += 1;
+			sprintf(tmpdata, "[one cannibal and one missionary sent to right bank]\n");
 		}
 		else if((right_bank.missionaries - (right_bank.cannibals + 1) >= 0 || right_bank.missionaries == 0) && left_bank.cannibals - 1 >= 0){ //send 2 cannibals to other side, if no side effect
 			left_bank.cannibals -= 1;
 			right_bank.cannibals += 1;
+			sprintf(tmpdata, "[One cannibal is sent to right side]\n");
 		}
 		else if((left_bank.missionaries - (left_bank.cannibals + 1) >= 0) && (left_bank.missionaries - 1 >= 0) && (right_bank.cannibals <= right_bank.missionaries + 1)){ //send 2 missinaries to other side, if no side effect
 			right_bank.missionaries += 1;
 			left_bank.missionaries -= 1;
+			sprintf(tmpdata, "[one missionary is sent to right bank]\n");
 		}
 		left_bank.boat = 0;
 		right_bank.boat = 1;
 	}
+	FILE *fp = fopen("rules", "a");
+	fprintf(fp,"%s", tmpdata);
+	fclose(fp);
 }
 int possible(){
 	if(d >= 4)
@@ -112,18 +129,24 @@ int main(int argc, char *argv[]){
 		if((right_bank.missionaries == atoi(argv[1])) && (right_bank.cannibals == atoi(argv[1])))
 			break;
 		nextStep();
-		printf("| %d | %d |\t| %d | %d |\n", left_bank.missionaries, left_bank.cannibals, right_bank.missionaries, right_bank.cannibals);
+		printf("| %d | %d |\t| %d | %d |", left_bank.missionaries, left_bank.cannibals, right_bank.missionaries, right_bank.cannibals);
 		fprintf(fp, "| %d | %d |\t| %d | %d |\n", left_bank.missionaries, left_bank.cannibals, right_bank.missionaries, right_bank.cannibals);
 		if(d == 2 && atoi(argv[1]) == 3 && i == 4){
 			flag = 1;
+			printf("%s", tmpdata);
 			break;
 		}
+		printf("%s", tmpdata);
 		i++;
 	}
 	fclose(fp);
 	if(flag == 1){
+		FILE *fp = fopen("rules", "a");
+		fprintf(fp,"%s", "[one missionary and one cannibal sent to left bank]\n");
+		fclose(fp);
 		system("./support.sh");
 	}
 	system("rm temp.txt");
+	system("rm rules");
 	return 0;
 }
--- AI/support.sh
+++ AI/support.sh
@@ -5,7 +5,9 @@ for i in `seq 1 $len`;
 do
     d=`expr $len - $j`
     j=`expr $j + 1`
-	cat temp.txt | head -$d | tail -1 >> test
+	cat temp.txt | head -$d | tail -1 | tr -d '\n' >> test
+	echo "	" | tr -d '\n' >> test
+	cat rules | head -$d | tail -1 >> test
 done
-awk -F'\t' '{print $2 "\t" $1}' test
+awk -F'\t' '{print $2 "\t" $1 $3}' test
 rm test
