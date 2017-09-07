e5a8e96dee27e9a37aa92a3210e8a9f3cc448b73
Tushar Agey <agey.tushar3@gmail.com>
2017-09-07 22:14:56 +0530

    initiated similarity check algorithms

--- Info_Retrieval/main.py
+++ Info_Retrieval/main.py
@@ -1,4 +1,4 @@
-#from stemming.porter2 import stem
+from stemming.porter2 import stem
 from os import listdir
 from os.path import isfile, join
 from operator import itemgetter
@@ -21,7 +21,7 @@ for file in files: #for each file do
 		if word.lower() not in stopwords: #remove stopwords
 			new_words.append(word.lower())
 	for word in new_words: #do stemming
-		newest_words.append(word)
+		newest_words.append(stem(word))
 	new_words = []
 	for word in newest_words: #remove stopwords
 		if word not in stopwords:
@@ -38,7 +38,19 @@ for file in files: #for each file do
 				inverted_index[word].append([file, new_words.count(word)])
 	unique_word_list = []
 	new_words = []
+flag = 0 #adding entries of files with 0 count of words
+tf_index_on_disk = inverted_index
+for file in files:
+	for word in tf_index_on_disk:
+			for element in tf_index_on_disk[word]:
+				if file in element:
+					flag = 1
+					break
+			if flag == 0:
+				tf_index_on_disk[word].append([file, 0])
+			flag = 0
 x = 0
+new_index = {}
 for i in sorted(inverted_index):
 	print i,
 	d = 10 - len(i)
@@ -46,4 +58,8 @@ for i in sorted(inverted_index):
 		print "",
 		x = x + 1
 	x = 0
+	new_index[i] = sorted(inverted_index[i], key=itemgetter(0), reverse=True)
 	print sorted(inverted_index[i], key=itemgetter(1), reverse=True)
+write_file = open("./input/tf_model", "w")
+write_file.write(str(new_index))
+write_file.close()
