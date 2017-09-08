722f171c2455221d8b106d8cdcd774d7eec8dbc0
Tushar Agey <agey.tushar3@gmail.com>
2017-09-08 12:45:05 +0530

    slight modifications in similarity measure calculations

--- Info_Retrieval/similarity_measures.py
+++ Info_Retrieval/similarity_measures.py
@@ -16,7 +16,7 @@ def buildTfIdfModelFromTfModel():
 		for element in tf_vector_model[term]:
 			if 0 not in element: #term exists in the document
 				num += 1
-		idf[term] = log10(len(tf_vector_model)/num)
+		idf[term] = log10((len(tf_vector_model[term])*1.0)/(num*1.0))
 		num = 0
 	#save this idf for processing the query too.
 	idf_file = open("./input/idf_file", "w")
@@ -31,7 +31,15 @@ def buildTfIdfModelFromTfModel():
 				tf_idf[term] = []
 				tf_idf[term].append([element[0], element[1] * idf[term]])
 	return tf_idf
-	
+
+def build_tfidf_of_query(query):
+	idf = eval(open("./input/idf_file", "r").read())
+	print idf
+	tf_idf_of_query = {}
+	for word in query.split():
+		if word in idf:
+			pass
+			
 def innerProduct(query, document_dict): #here onwards, the similarity checks are in operation. query is the string to be searched and document_dict is tf-idf measure
 	pass
 
@@ -48,7 +56,7 @@ def CosineSimilarity(query, document_dict):
 	pass
 
 def preProcess(): #this file returnes the tf-idf if it already exists, else returns -1 suggesting invocation buildTfIdfModelFromTfModel
-	if os.path.exists('./input/tf_modelDemo'):
+	if os.path.exist/'):
 		return buildTfIdfModelFromTfModel()
 	print "please create the tf-model using main.py script."
 	return -1
