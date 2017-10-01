5bf111fc86bebb8bde00ffb90e2eb48a1a2b8e2b
Tushar Agey <agey.tushar3@gmail.com>
2017-10-01 07:55:25 +0530

    improved AOStar algorithm

--- AI/search_algos/home/AOStar.py
+++ AI/search_algos/home/AOStar.py
@@ -1,31 +1,132 @@
 import json
-def giveSmallestNeighbour(elem, heuristics, edges, and_node_info, closed, opened):#look at all the neighbours of elements in opened list and find neighbour with least f(x) = h(x) + g(x)
-	#remove the elements common to and_node_info and my neighbours from the my_neighbours list
+def findChildrensAndGiveMyDistances(elem, opened, eh, andNodes): #finds childrens and returns distances & add childrens to opened
+	elemList = eh[ord(elem) - ord('A')]
+	i = 0
+	dictRet = []
+	for el in elemList: #for OR of the nodes
+		elist = []
+		try:
+			elist = andNodes[elem]
+		except Exception as e:
+			elist = []
+		if (i is not ord(elem) - ord('A')) and (el is not -1) and (chr(i + ord('A')) not in elist):
+			dictRet.append([chr(i + ord('A')), eh[ord(elem) - ord('A')][i] + eh[ord(elem) - ord('A')][ord(elem) - ord('A')]])
+			opened.append(chr(i + ord('A')))
+		i += 1
+	if elem in andNodes: #I have AND nodes
+		sum = 0
+		for data in andNodes[elem]:
+			opened.append(data)
+			sum += eh[ord(elem) - ord('A')][ord(data) - ord('A')] + eh[ord(data) - ord('A')][ord(data) - ord('A')]
+		andNodes[elem].append(sum)
+		dictRet.append(andNodes[elem])
+	return dictRet, opened
 
-	#and substitute it with their combined Element
-	#then find f(x) = h(x) + g(x)
-def aoStar(input):
-	visited = []
-	heuristicVals = []
-	for elem in input['heuristics']:
-		heuristicVals.append(elem.values()[0])
-	for elem in input['heuristics']:
-		for data in elem:
-			if elem[data] == 0:
-				goal = data
-	start = 'A' #considering A as start node always & element with 0 heuristic as goal node
-	finalPath = []
-	finalPath.append(start)#get the start node.
+def updateMyHeuristics(myDictionary, N, eh, goal): #updates distances using childrens 
+	N = N - 1
+	while N is not -1:
+		i = 0
+		elem = chr(ord('A') + N)
+		childrens = []
+		for data in eh[N]:
+			if data is not -1 and N is not i:
+				childrens.append(chr(ord('A') + i))
+			i += 1
+		flag = True
+		if elem in myDictionary and len(childrens) is not 0: #the element is in dictionary
+			for data in childrens:	#all its childrens are in dictionary
+				if data not in myDictionary:
+					flag = False
+					break
+			if flag: #True
+				i = 0
+				for eachData in myDictionary[elem]:
+					sum = 0
+					anotherMin = []
+					if goal in eachData:
+						myDictionary[goal] = [['F', 0]]
+					if(len(eachData) == 2): #eachData is a OR node
+						for newData in myDictionary[eachData[0]]:
+							anotherMin.append(newData[len(newData) - 1])
+						myDictionary[elem][i][1] = min(anotherMin) + eh[ord(elem) - ord('A')][ord(eachData[0]) - ord('A')]
+					else: #eachData is an AND node
+						sum = 0
+						for j in range(len(eachData) - 1):
+							anotherMin = []
+							for newData in myDictionary[eachData[j]]:
+								anotherMin.append(newData[len(newData) - 1])
+							sum += min(anotherMin) + eh[ord(elem) - ord('A')][ord(eachData[j]) - ord('A')]
+						myDictionary[elem][i][len(eachData) - 1] = sum
+					i += 1
+		N -=1
+	return myDictionary
+
+def AOStar(eh, N, andNodes):
 	opened = []
 	closed = []
-	curr = start
-	opened.append(start)#add the current node to opened list
+	nextOpened = []
+	hm = []
+	myDictionary = {}
+	start = 'A'
+	goal = chr((len(eh) - 1) + ord('A'))
+	print goal
+	opened.append(start)
+	nextOpened = opened
+	x = 0
+	while goal not in closed:
+		for elem in opened:
+			myDictionary[elem], hm = findChildrensAndGiveMyDistances(elem, nextOpened, eh, andNodes)
+			opened.remove(elem)
+			for eachNextChild in hm:
+				if eachNextChild not in nextOpened and eachNextChild not in closed:
+					nextOpened.append(eachNextChild)
+			closed.append(elem) #the elem is processed. Hence close it
+		opened = nextOpened
+		nextOpened = []
+		for elem in closed:
+			flag = True
+			i = 0
+			childrens = []
+			for data in eh[ord(elem) - ord('A')]:
+				if data is not -1 and ord(elem) - ord('A') is not i:
+					childrens.append(chr(ord('A') + i))
+				i += 1
+			for data in childrens: #if all my childrens are either in myDictionary, i.e. processed:
+				if data not in myDictionary:
+					flag = False
+					break
+			if flag:
+				myDictionary = updateMyHeuristics(myDictionary, N, eh, goal)
+				#print myDictionary
+				#break
+	#printing the path from start to goal node!
+	#print myDictionary
+	finalPath = []
+	finalPath.append(start)
 	while goal not in finalPath:
-		curr,closed,opened = giveSmallestNeighbour(curr, heuristicVals, input['edges'], input['ands'], closed, opened)
-		finalPath.append(curr)#Add the selected one to finalPath list
-js=open('./data/input.json')
+		previous = finalPath[len(finalPath) - 1]
+		minimum = previous
+		minVal = 1000000
+		for elem in myDictionary[previous]:
+			if minVal > elem[len(elem) - 1]:
+				minVal = elem[len(elem) - 1]
+				minimum = elem[0]
+		if minimum not in finalPath:
+			finalPath.append(minimum)
+	return finalPath
+
+js=open('./data/AOStarInput.json')
 data=json.load(js)
 finalPath = {"path" : []}
-finalPath['path'] = aoStar(data)
+heuristicVals = []
+for elem in data['heuristics']:
+	heuristicVals.append(elem.values()[0])
+eh = data['edges']
+i = 0
+for elem in heuristicVals:
+	eh[i][i] = elem
+	i += 1
+finalPath['path'] = AOStar(eh, data['N'], data['ands'])
+print finalPath['path']
 with open('./data/AO.json', 'w') as fp:
     json.dump(finalPath, fp)
